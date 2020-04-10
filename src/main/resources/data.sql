eca706156c7d0e3cf3bc62caca6b968ef89d6e7d
kzk <kuangzk@jec.com.cn>
2020-04-10 17:00:56 +0800

    jdbc

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+Insert into `Test` values ('kzk','123456')
+Insert into `Test` values ('kjj','123456')
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+Drop table If exists `Test`;
+create table Test(
+name char(20) primary key,
+old int
+);
